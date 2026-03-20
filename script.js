cdd372cc840490bce6acb93644b7d06f462caddf
Devyani_G <gorakshadevyani@gmail.com>
2026-03-20 22:29:14 +0530

    Update and rename style.js to script.js

--- script.js
+++ script.js
@@ -60,3 +60,38 @@ function escapeHTML(str) {
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;");
 }
+// COPY
+function copyCode(index) {
+  const code = snippets[index].code;
+
+  navigator.clipboard.writeText(code);
+
+  let toast = document.getElementById("toast");
+  toast.classList.add("show");
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+}
+
+// DELETE
+function deleteSnippet(index) {
+  snippets.splice(index, 1);
+  localStorage.setItem("snippets", JSON.stringify(snippets));
+  displaySnippets();
+}
+
+// SEARCH
+function searchSnippet() {
+  let query = document.getElementById("search").value.toLowerCase();
+
+  let filtered = snippets.filter(s =>
+    s.title.toLowerCase().includes(query) ||
+    s.code.toLowerCase().includes(query)
+  );
+
+  displaySnippets(filtered);
+}
+
+// INIT
+displaySnippets();
